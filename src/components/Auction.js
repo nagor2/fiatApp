58a7383e1abe08ae8748ee054b2e4afc7f7c69a5
Дмитрий Нагорных <dmitriy.stepnoy@demlabs.net>
2025-12-27 14:27:18 +0700

    fix: added input to allow tokens

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -8,6 +8,7 @@ export default class Auction extends React.Component{
         super(props);
         this.state = {
             id: 0,
+            toAllow:0,
             allowanceToAuction: 0,
             auction: 0,
             paymentToken: '',
@@ -164,11 +165,12 @@ export default class Auction extends React.Component{
 
             <div>your {this.state.paymentToken} allowance to auction: {parseFloat(this.state.allowanceToAuction)/10**18}</div>
 <div>paymentBalance: {parseFloat(this.state.paymentBalance)/10**18}, paymentAmount: {parseFloat(this.state.auction.paymentAmount)/10**18}, bool: {(this.state.paymentBalance>=this.state.auction.paymentAmount)}</div>
-            {(parseFloat(this.state.paymentBalance)>=parseFloat(this.state.auction.paymentAmount))?<a className={"small-button pointer green right"}
-                                                                              onClick={()=>this.allowPayment()}>Allow {parseFloat(this.state.auction.paymentAmount)>0?this.state.auction.paymentAmount/10**18:this.state.nextBid} {this.state.paymentToken}</a>:<div className="small-button address right">
-                {'not enough '+this.state.paymentToken+' to participate in this auction'}</div>}
+            {(parseFloat(this.state.paymentBalance)>=parseFloat(this.state.auction.paymentAmount))?<input type='number' step="0.1" min={this.state.nextBid} max="10000" name='allowance' onChange={e => this.state.toAllow = this.props.web3.utils.toWei(e.target.value)}/><a className={"small-button pointer green right"}
+                   onClick={() => this.allowPayment()}>Allow {parseFloat(this.state.toAllow)/10**18} {this.state.paymentToken}</a>:<div className="small-button address right">
+            {'not enough '+this.state.paymentToken+' to participate in this auction'}</div>
+    }
 
-            <div>address:         <a target='_blank' href={'https://blockscout.com/etc/mainnet/address/'+this.state.address}>{this.state.address}</a></div>
+        <div>address: <a target='_blank' href={'https://blockscout.com/etc/mainnet/address/'+this.state.address}>{this.state.address}</a></div>
 
             <div>code:         <a target='_blank' href={'https://blockscout.com/etc/mainnet/address/'+this.state.address+'/contracts#address-tabs'}>view code</a></div>
 
